c7cba3b0900e94c14e0556489c42412feeaa77c1
DevopsGovardhan <m.govardhanreddy786@gmail.com>
2019-12-05 17:17:38 +0530

    centosimage

--- centoscontainer.sh
+++ centoscontainer.sh
@@ -2,7 +2,7 @@ docker pull centos
 docker run -itd --name myserver  docker.io/centos /bin/bash
 docker ps
 docker ps -a
-docker exec myserver yum update
+docker exec myserver yum update -y
 docker exec myserver cat /etc/os-release
 docker exec myserver yum install -y
 docker exec myserver git clone https://github.com/DevopsGovardhan/docker.git
