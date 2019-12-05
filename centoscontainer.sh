4deb626c8c184449d6bae3b69ad9523123d135eb
DevopsGovardhan <m.govardhanreddy786@gmail.com>
2019-12-05 17:41:50 +0530

    centos image

--- centoscontainer.sh
+++ centoscontainer.sh
@@ -4,7 +4,8 @@ docker ps
 docker ps -a
 docker exec myserver yum update -y
 docker exec myserver cat /etc/os-release
-docker exec myserver yum install -y
-docker exec myserver git clone https://github.com/DevopsGovardhan/docker.git
+docker exec myserver yum install git -y
+docker exec myserver git clone https://github.com/DevopsGovardhan/saiproject.git
+
 docker exec myserver cd /Docker && touch a b c d e f
 docker exec myserver cd /Docker && git add . && git commit -m "anymessage"
