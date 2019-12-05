b20c0a41775bb7f46f90c9e1fecab179e79eb27e
DevopsGovardhan <m.govardhanreddy786@gmail.com>
2019-12-05 17:00:36 +0530

    docker commands

---
+++ centoscontainer.sh
@@ -0,0 +1,10 @@
+docker pull centos
+docker run -itd --name myserver  docker.io/centos /bin/bash
+docker ps
+docker ps -a
+docker exec myserver yum update
+docker exec myserver cat /etc/os-release
+docker exec myserver yum install -y
+docker exec myserver git clone https://github.com/DevopsGovardhan/docker.git
+docker exec myserver cd /Docker && touch a b c d e f
+docker exec myserver cd /Docker && git add . && git commit -m "anymessage"
