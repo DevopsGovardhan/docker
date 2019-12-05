a8106cb2f4fba53d9fe1326a224cf7fc6908dc2d
DevopsGovardhan <m.govardhanreddy786@gmail.com>
2019-12-05 18:59:54 +0530

    centosimage

--- centoscontainer.sh
+++ centoscontainer.sh
@@ -6,6 +6,7 @@ docker exec myserver yum update -y
 docker exec myserver cat /etc/os-release
 docker exec myserver yum install git -y
 docker exec myserver git clone https://github.com/DevopsGovardhan/saiproject.git
-
+docker exec myserver cd /saiproject && git config --global user.name "DevopsGovardhan"
+docker exec myserver cd /saiproject && git config --global user.email "m.govardhanreddy786@gmail.com"
 docker exec myserver cd /saiproject && touch a b c d e f
 docker exec myserver cd /saiproject && git add . && git commit -m "anymessage"
